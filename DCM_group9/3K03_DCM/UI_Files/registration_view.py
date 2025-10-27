edd8d208b76e9a793abbef4ebfaf75d2a4b54d48
Farhan-sifar <fxrhan.s13@gmail.com>
2025-10-27 04:13:25 -0400

    aa

--- DCM_group9/3K03_DCM/UI_Files/registration_view.py
+++ DCM_group9/3K03_DCM/UI_Files/registration_view.py
@@ -27,9 +27,7 @@ class WelcomeFrame(ttk.Frame):
         self._clear_content()
 
         title = ttk.Label(self._content, text="3K04 Device Controller-Monitor", font=("Segoe UI", 20, "bold"))
-        subtitle = ttk.Label(self._content, text="Deliverable 1 - Front-End Only", font=("Segoe UI", 12))
         title.grid(row=0, column=0, pady=(0,8))
-        subtitle.grid(row=1, column=0, pady=(0,20))
 
         # Login UI (only username/password and register butotn)
         login_card = ttk.Frame(self._content, style="Card.TFrame", padding=16)
